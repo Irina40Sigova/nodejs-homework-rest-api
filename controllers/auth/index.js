ccfb8f5653542232cd898091cbcb14ddbc583a00
Irina40Sigova <mishinaira1@gmail.com>
2023-07-03 18:20:33 +0300

    init

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,6 +2,10 @@ const { ctrlWrapper } = require("../../helpers");
 
 const register = require("./register");
 
+const verifyEmail = require("./verifyEmail");
+
+const resendVerifyEmail = require("./resendVerifyEmail");
+
 const login = require("./login");
 
 const getCurrent = require("./getCurrent");
@@ -12,6 +16,8 @@ const updateAvatar = require("./updateAvatar");
 
 module.exports = {
   register: ctrlWrapper(register),
+  verifyEmail: ctrlWrapper(verifyEmail),
+  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,9 +1,11 @@
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
 
 const { User } = require("../../models/user");
 
-const { HttpError } = require("../../helpers");
+const { HttpError, sendEmail } = require("../../helpers");
+const { BASE_URL } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -15,12 +17,22 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
 
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify token is required",
+    html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Confirm your email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     email: newUser.email,
